20079e8d8d0e2d0497214f8ef547fc688b4e3426
watchmypizza <kevinosswald@proton.me>
2025-01-26 22:24:08 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+/* Register Clicking Event on Navbar Links */
+document.getElementById('github').addEventListener('click', () => {
+    /* Open a new tab with the GitHub repository */
+    window.open('https://github.com/watchmypizza/IPA-Storage', '_blank');
+});
